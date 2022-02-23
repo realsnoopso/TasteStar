5ce43db353c958372fedecee5429c86127fc298a
realsnoop <96381221+realsnoop@users.noreply.github.com>
2022-02-23 23:43:49 +0900

    fix: svg 이미지 업로드 안되는 문제 해결, flask - mangoDB 연결

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
+# flask setting
 from flask import Flask, render_template
 app = Flask(__name__)
+
+# mongoDB setting
+import certifi
+import pymongo
+ca = certifi.where()
+client = pymongo.MongoClient("mongodb+srv://sparta19:toyproject@cluster0.rrg9h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", tlsCAFile=ca)
+db = client.TasteStar
+
+# scraping setting
+import requests
+from bs4 import BeautifulSoup
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 
 @app.route('/')
 def home():
