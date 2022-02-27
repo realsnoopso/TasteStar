63d95e8a24726b59b4c9a9f029a9ff03fc244f5b
realsnoop <96381221+realsnoop@users.noreply.github.com>
2022-02-27 16:51:23 +0900

    feat: 댓글 기능 수정중

--- app.py
+++ app.py
@@ -1,20 +1,65 @@
-# flask setting
-from flask import Flask, render_template, request, jsonify, json
+from flask import Flask, render_template, request, jsonify
 import certifi
-import time
 from pymongo import MongoClient
-app = Flask(__name__)
+import time
 
-# mongoDB setting
-import certifi
-import pymongo
 ca = certifi.where()
-client = pymongo.MongoClient("mongodb+srv://sparta19:toyproject@cluster0.rrg9h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", tlsCAFile=ca)
+client = MongoClient("mongodb+srv://sparta19:toyproject@cluster0.rrg9h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", tlsCAFile=ca)
 db = client.TasteStar
+app = Flask(__name__)
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 
 @app.route('/')
 def home():
-   return render_template('index.html')
+    return render_template('index.html')
+
+@app.route('/detail/detailreview')
+def detailreview():
+    return render_template('review.html', id=request.args.get('id'))
+
+@app.route("/resDetail", methods=["GET"])
+def res_Detail():
+    resDetail = db.restaurants.find_one({'mangoID' : request.args.get('mangoId')},{'_id':False})
+    imgList = list(db.imgs.find({'mangoID' : request.args.get('mangoId')},{'_id':False}))
+    return jsonify({'resDetail' : resDetail, 'imgList' : imgList})
+
+@app.route("/detail/resDetail", methods=["GET"])
+def review_get():
+    rows = list(db.reviews.find({"pageID":request.args.get('mangoId')}, {'_id': False}))
+    bad = len(list(db.reviews.find({"score":"별로였어요","pageID":request.args.get('mangoId')}, {'_id': False})))
+    soso = len(list(db.reviews.find({"score":"괜찮았어요","pageID":request.args.get('mangoId')}, {'_id': False})))
+    good = len(list(db.reviews.find({"score":"맛있었어요","pageID":request.args.get('mangoId')}, {'_id': False})))
+    total = len(rows)
+
+    pageid = db.reviews.find({"pageID": request.args.get('mangoId')}, {'_id': False})
+
+    return jsonify({'rows': rows,
+                    'bad' : bad,
+                    'soso' : soso,
+                    'good' : good,
+                    'total': total,
+                    'pageid': pageid
+                    })
+
+@app.route("/detail/resDetail", methods=["POST"])
+def review_post():
+    review_nickname_receive = request.form['review_nickname_give']
+    review_comment_receive = request.form['review_comment_give']
+    emotion_receive = request.form['emotion_give']
+    pageid = request.form['pageid']
+
+    now = time.localtime()
+    detail_now = "%04d/%02d/%02d %02d:%02d:%02d"%(now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec)
+
+    doc = {
+        'nickname': review_nickname_receive,
+        'comment' : review_comment_receive,
+        'score' : emotion_receive,
+        'time' : detail_now,
+        'pageid': pageid
+    }
+    db.reviews.insert_one(doc)
+    return jsonify({'msg': '댓글이 추가되었습니다!!'})
 
 # 구별 링크
 @app.route('/detail')
@@ -26,7 +71,7 @@ def detail():
 def district() :
    searchDist = request.args.get("district")
    district_list = list(db.restaurants.find({'district':searchDist}, {'_id':False}).sort('starTotal', -1))
-   result = [];
+   result = []
    for dining in district_list :
       item = {}
       item["restaurant_name"] = dining['name']
@@ -42,51 +87,5 @@ def district() :
       result.append(item)
    return jsonify({'result': result})
 
-## 태성 code ##
-@app.route('/detail/resDetail', methods=["GET"])
-def detailPage():
-    id = request.args.get('id')
-    return render_template('review.html', id = id)
-
-@app.route("/resDetail", methods=["GET"])
-def res_Detail():
-    resDetail = db.restaurants.find_one({'mangoID' : request.args.get('mangoId')},{'_id':False})
-    imgList = list(db.imgs.find({'mangoID' : request.args.get('mangoId')},{'_id':False}))
-    return jsonify({'resDetail' : resDetail, 'imgList' : imgList})
-
-@app.route("/review", methods=["GET"])
-def review_get():
-    rows = list(db.reviews.find({}, {'_id': False}))
-    bad = len(list(db.reviews.find({"score":"별로였어요"}, {'_id': False})))
-    soso = len(list(db.reviews.find({"score":"괜찮았어요"}, {'_id': False})))
-    good = len(list(db.reviews.find({"score":"맛있었어요"}, {'_id': False})))
-    total = len(rows)
-
-    return jsonify({'rows': rows,
-                    'bad' : bad,
-                    'soso' : soso,
-                    'good' : good,
-                    'total': total})
-
-@app.route("/review", methods=["POST"])
-def review_post():
-    review_nickname_receive = request.form['review_nickname_give']
-    review_comment_receive = request.form['review_comment_give']
-    emotion_receive = request.form['emotion_give']
-
-    now = time.localtime()
-    detail_now = "%04d/%02d/%02d %02d:%02d:%02d"%(now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec)
-
-    doc = {
-        'nickname': review_nickname_receive,
-        'comment' : review_comment_receive,
-        'score' : emotion_receive,
-        'time' : detail_now
-    }
-    db.reviews.insert_one(doc)
-
-    return jsonify({'msg': '댓글이 추가되었습니다!!'})
-
-
 if __name__ == '__main__':
-   app.run('0.0.0.0',port=8000,debug=True)
+    app.run('0.0.0.0', port=8000, debug=True)
